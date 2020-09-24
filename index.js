bdcf14a5bbd1c6d612c0614275612cc66b1d37f0
ilkaydnc <ilkaydinc1@gmail.com>
2020-09-25 00:02:40 +0300

    Create simple tmi.js connection

---
+++ index.js
@@ -0,0 +1,29 @@
+const tmi = require("tmi.js");
+const { username, oauth, channels } = require("./config");
+
+const options = {
+  options: {
+    debug: false,
+  },
+  connection: {
+    reconnect: true,
+  },
+  identity: {
+    username,
+    password: oauth,
+  },
+  channels,
+};
+
+const client = new tmi.client(options);
+
+client
+  .connect()
+  .then((data) => console.log("connected", data))
+  .catch((err) => {
+    throw new Error(err);
+  });
+
+client.on("message", (channel, args, message, self) => {
+  console.log(channel, message);
+});
